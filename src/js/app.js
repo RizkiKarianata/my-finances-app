ce3e5665354e9d6ca39d5e964e232f398952bafe
Rizki Karianata <natarizky884@gmail.com>
2022-06-26 18:45:59 +0700

    chore: change theme to ios

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,7 @@ var app = new Framework7({
   component: App, // App main component
   id: 'io.framework7.myapp', // App bundle ID
   name: 'Crossword Puzzle', // App name
-  theme: 'auto', // Automatic theme detection
+  theme: 'ios', // Automatic theme detection
 
 
 
