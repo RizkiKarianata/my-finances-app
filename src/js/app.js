ac6779fed3481a6f6d9941954d07c8cc27e0faaf
Rizki Karianata <natarizky884@gmail.com>
2022-06-26 18:46:15 +0700

    refactor: change theme to android

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,7 @@ var app = new Framework7({
   component: App, // App main component
   id: 'io.framework7.myapp', // App bundle ID
   name: 'Crossword Puzzle', // App name
-  theme: 'ios', // Automatic theme detection
+  theme: 'android', // Automatic theme detection
 
 
 
